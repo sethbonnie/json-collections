765cbc8755d715e859ad21d6d312b0b45f067773
Seth Bonnie <sethkbonnie@gmail.com>
2015-08-12 00:14:05 -0500

    add model

---
+++ src/model.js
@@ -0,0 +1,86 @@
+var Immutable = require( 'immutable' );
+var Map = Immutable.Map;
+var List = Immutable.List;
+
+/**
+  * 
+  */
+var Model = module.exports = function (map, collection, cache) {
+  var model;
+
+  if ( Map.isMap(map) ) {
+    throw new Error( 'Missing map argument to Model constructor.' );
+  }
+
+  if ( List.isList(collection) ) {
+    throw new Error( 'Missing collection argument to Model constructor.' );
+  }
+
+  if ( List.isList(cache) ) {
+    throw new Error( 'Missing cache argument to Model constructor.' );
+  }
+
+  model = {
+    /**
+      * 
+      */
+    set: function( key, value ) {
+      var newMap;
+
+      if ( typeof key === 'string' ) {
+        newMap = map.set( key, value );
+      }
+      else if ( typeof key === 'object' ) {
+        newMap = map.merge( key );
+      }
+
+      updateCache( map, newMap, cache );
+
+      map = newMap;
+
+      return model;
+    },
+
+    /**
+      * 
+      */
+    remove: function( key ) {
+      var newMap;
+
+      newMap = map.delete( key );
+
+      updateCache( map, newMap, cache );
+
+      map = newMap;
+
+      return model;
+    },
+
+    /**
+      * @returns a promise that is executed after a successful write to disk.
+      */
+    save: function() {
+      return collection.persist();
+    },
+
+    /**
+      *
+      */
+    toJSON: function() {
+      return map.toJSON();
+    }
+  };
+
+  return model;
+};
+
+/**
+  * Updates the `oldMap` in `list` with `newMap`
+  */
+function updateCache( oldMap, newMap, list ) {
+  var index = list.indexOf( oldMap );
+
+  if ( index > -1 ) {
+    list = list.set( index, newMap );
+  }
+};
