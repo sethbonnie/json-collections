7ba100cec4963d7f3c9ef1657afcf8419ad15b85
Seth Bonnie <sethkbonnie@gmail.com>
2015-08-23 00:08:23 -0500

    add better comments to model

--- src/model.js
+++ src/model.js
@@ -3,14 +3,19 @@ var Map = Immutable.Map;
 var List = Immutable.List;
 
 /**
-  * Updates the `oldMap` in  with `newMap`
+  * Replaces the oldItem in collection with newItem.
+  * @param {Map} oldItem - The item to be replaced, is used to find the index
+  *   to be updated in the collection.
+  * @param {Map} newItem
+  * @param {Object} collection - The collection in which we'll swap the items.
+  * @returns {Object} The updated state of the collection.
   */
-function updateCache( oldMap, newMap, collection ) {
+function updateCache( oldItem, newItem, collection ) {
   var state = collection._getState();
-  var index = state.indexOf( oldMap );
+  var index = state.indexOf( oldItem );
 
   if ( index > -1 ) {
-    state = state.set( index, newMap );
+    state = state.set( index, newItem );
   }
 
   collection._update(state);
@@ -37,12 +42,20 @@ module.exports = function Model(map, collection, cache) {
   }
 
   model = {
+    /**
+      * Returns the value of given key in the model.
+      */
     get: function (key) {
       return map.get(key);
     },
 
     /**
-      * 
+      * @param {Object|String} - When a string, updates that key in the model
+      *   with the given `value`.
+      *
+      *   When an object, merges its key-value pairs with those in the model.
+      * @param {Any} (optional) value - The value to be updated in the model.
+      * @returns The model for chianing purposes.
       */
     set: function( key, value ) {
       var newMap;
@@ -62,7 +75,9 @@ module.exports = function Model(map, collection, cache) {
     },
 
     /**
-      * 
+      * Removes a key from the model. This change will be reflected
+      * in the model's collection.
+      * @returns {Object} - the model for chaining purposes.
       */
     remove: function( key ) {
       var newMap;
@@ -84,7 +99,8 @@ module.exports = function Model(map, collection, cache) {
     },
 
     /**
-      *
+      * Returns pure JSON version of the model.
+      * @returns {Object}
       */
     toJSON: function() {
       return map.toJSON();
